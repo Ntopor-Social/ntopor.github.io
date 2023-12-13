5114a72e94fd4454198589f969af946246f2faac
Jume <jumeb@Jumes-Mac.local>
2023-12-13 09:42:19 +0100

    Added a fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { NextPageWithLayout } from "./_app";
 import NavBar from "@/components/navbar/navbar.component";
 import Header from "@/section/Header/header.section";
@@ -16,9 +15,7 @@ import FAQ from "@/section/FAQ/faq.section";
 import Bills from "@/section/Bills/bills.section";
 import Testimonial from "@/section/Testimonials/testimonials.section";
 
-const Index: NextPageWithLayout = ({
-  ip,
-}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
+const Index: NextPageWithLayout = (): JSX.Element => {
   const position = UseScrollPosition();
   return (
     <div>
@@ -45,38 +42,5 @@ const Index: NextPageWithLayout = ({
     </div>
   );
 };
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
-  let ip = req.headers["x-real-ip"];
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  if (req.headers["x-forwarded-for"]) {
-    ip = req.headers["x-forwarded-for"].toString().split(",")[0];
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  } else if (req.headers["x-real-ip"]) {
-    ip = req.socket.remoteAddress;
-  } else {
-    ip = req.socket.remoteAddress;
-  }
-
-  // const result = await Promise.all([items]).then(([items]) => {
-  //   return { items };
-  // });
-
-  return {
-    props: {
-      // items: result.items,
-      ip,
-    },
-  };
-};
-
-// Index.getLayout = function getLayout(page: ReactElement) {
-//   return <div>{page}</div>;
-// };
 
 export default Index;
